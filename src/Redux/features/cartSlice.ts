e0993b35ed4b370fcb4af95f9bad8d5fe0a888e9
SujonHasan <sujonhasan171@gmail.com>
2023-12-15 18:33:53 +0600

    bootstrap, Redux toolkit setup

---
+++ src/Redux/features/cartSlice.ts
@@ -0,0 +1,14 @@
+'use client'
+
+import { createSlice } from "@reduxjs/toolkit"
+
+const cartSlice = createSlice({
+
+    name: "Cart",
+    initialState: [],
+    reducers:{
+
+    }
+})
+
+export default cartSlice.reducer;
